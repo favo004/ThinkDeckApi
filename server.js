2cc074a44be6ffe613ea15eab98833da402637de
favo004 <jfavo@hotmail.com>
2021-05-05 10:53:55 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 import express from 'express'
 
 require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+app.listen(() => {
+    console.log(`Listening on port ${port}`);
+})
