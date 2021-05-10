ddb987a21c346933b3b8ff925553233114b525a9
favo004 <jfavo@hotmail.com>
2021-05-10 08:15:57 -0400

    Added thoughts data to seed

---
+++ seed/thoughts.js
@@ -0,0 +1,17 @@
+export const thoughts = [
+    {
+        "userId": null,
+        "thoughtId": null,
+        "thought": "Weeeeee I love cheeeese!"
+    },
+    {
+        "userId": null,
+        "thoughtId": null,
+        "thought": "This thought is for something else!"
+    },
+    {
+        "userId": null,
+        "thoughtId": null,
+        "thought": "I think this is fun! Think'n is fun!"
+    }
+];
