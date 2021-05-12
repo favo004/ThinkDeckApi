e1d77ef76a4e7cc64aaafc11e37649c9e6680fa2
favo004 <jfavo@hotmail.com>
2021-05-12 14:09:43 -0400

    Added an environment check for logging

--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,20 @@
 import { format, transports, createLogger, config } from 'winston'
 
-export const logger = createLogger({
+export const logger = 
+process.env.NODE_ENV === 'test' ? 
+createLogger({
+    transports:
+        new transports.File({
+            filename: 'logs/test.log',
+            format: format.combine(
+                format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+                format.align(),
+                format.printf(info => `${info.level}: ${info.timestamp}: ${info.message}`)
+            )
+        })
+})
+:
+createLogger({
     transports:
         new transports.File({
             filename: 'logs/server.log',
