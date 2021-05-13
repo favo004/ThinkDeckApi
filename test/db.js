0da5e87c301b15c5c33ac439fd399a68386b3dfd
favo004 <jfavo@hotmail.com>
2021-05-13 10:51:35 -0400

    Added analytics seeding.

--- test/db.js
+++ test/db.js
@@ -3,11 +3,11 @@ import {ObjectId} from 'bson';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { UserSchema } from '../models/userModel';
 import { ThoughtSchema } from '../models/thoughtModel';
+import { DislikeSchema, HighlightSchema, LikeSchema } from '../models/analyticsModel';
 
 const mongoServer = new MongoMemoryServer();
 
-const User = mongoose.model('User', UserSchema);
-const Thought = mongoose.model('Thought', ThoughtSchema);
+
 
 export const connectDb = async () => {
     
@@ -24,31 +24,31 @@ export const connectDb = async () => {
     })
 }
 
-let testUsers = [
+export const testUsers = [
     {
         "_id": ObjectId(),
         "username": "Jarsher",
-        "password": "$2a$08$wilSc/9mGDQYiDRGDjFjR.2tba3XM5227IBI5WDqIroLRtp.iRgue",
+        "password": "password",
         "email": "jarsher@email.com",
         "bio": "Here we go again! WOOOT"
     },
     {
         "_id": ObjectId(),
         "username": "Hooplah",
-        "password": "$2a$08$wilSc/9mGDQYiDRGDjFjR.2tba3XM5227IBI5WDqIroLRtp.iRgue",
+        "password": "password",
         "email": "hooplah@email.com",
         "bio": "I am the best, around! No one is ever going to bring me down!"
     },
     {
         "_id": ObjectId(),
         "username": "Marsh",
-        "password": "$2a$08$wilSc/9mGDQYiDRGDjFjR.2tba3XM5227IBI5WDqIroLRtp.iRgue",
+        "password": "password",
         "email": "marsh@email.com",
         "bio": ""
     }
 ]
 
-let testThoughts = [
+export const testThoughts = [
     {
         "_id": ObjectId(),
         "user": testUsers[0],
@@ -71,18 +71,79 @@ let testThoughts = [
     },
 ]
 
+export const testLikes = [
+    {
+        "user": testUsers[0],
+        "thought": testThoughts[0]
+    },
+    {
+        "user": testUsers[1],
+        "thought": testThoughts[0]
+    },
+    {
+        "user": testUsers[2],
+        "thought": testThoughts[0]
+    },
+    {
+        "user": testUsers[0],
+        "thought": testThoughts[3]
+    }
+]
+
+export const testDislikes = [
+    {
+        "user": testUsers[0],
+        "thought": testThoughts[2]
+    },
+    {
+        "user": testUsers[1],
+        "thought": testThoughts[1]
+    },
+    {
+        "user": testUsers[2],
+        "thought": testThoughts[1]
+    },
+    {
+        "user": testUsers[1],
+        "thought": testThoughts[3]
+    }
+]
+
+export const testHighlights = [
+    {
+        "user": testUsers[1],
+        "thought": testThoughts[0]
+    },
+    {
+        "user": testUsers[2],
+        "thought": testThoughts[0]
+    }
+]
+
 export const seedTestData = async () => {
+    const User = mongoose.model('User', UserSchema);
+    const Thought = mongoose.model('Thought', ThoughtSchema);
+    const Like = mongoose.model('Like', LikeSchema);
+    const Dislike = mongoose.model('Dislike', DislikeSchema);
+    const Highlight = mongoose.model('Highlight', HighlightSchema);
 
     await Promise.all([
         User.deleteMany({}),
-        Thought.deleteMany({})
+        Thought.deleteMany({}),
+        Like.deleteMany({}),
+        Dislike.deleteMany({}),
+        Highlight.deleteMany({})
     ])
 
-    testThoughts[3].thought = testThoughts[0];
+    // Set thought as comment
+    testThoughts[3].commentTo = testThoughts[0];
 
     await Promise.all([
         User.create(testUsers),
-        Thought.create(testThoughts)
+        Thought.create(testThoughts),
+        Like.create(testLikes),
+        Dislike.create(testDislikes),
+        Highlight.create(testHighlights)
     ])
 
 }
