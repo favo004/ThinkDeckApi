77b8e2ff08d73a8c656af1c8e301be4f031beffa
favo004 <jfavo@hotmail.com>
2021-05-18 13:54:35 -0400

    Updated seed thought data to new model params

--- seed/thoughts.js
+++ seed/thoughts.js
@@ -2,16 +2,16 @@ export const thoughts = [
     {
         "user": null,
         "commentTo": null,
-        "thought": "Weeeeee I love cheeeese!"
+        "thoughtBody": "Weeeeee I love cheeeese!"
     },
     {
         "user": null,
         "commentTo": null,
-        "thought": "This thought is for something else!"
+        "thoughtBody": "This thought is for something else!"
     },
     {
         "user": null,
         "commentTo": null,
-        "thought": "I think this is fun! Think'n is fun!"
+        "thoughtBody": "I think this is fun! Think'n is fun!"
     }
 ];
