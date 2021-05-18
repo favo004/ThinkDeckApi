e98057b120cbef166df895a00cb86905bf32782f
favo004 <jfavo@hotmail.com>
2021-05-18 13:54:09 -0400

    Changed seed data user passwords from pre-hashed

--- seed/users.js
+++ seed/users.js
@@ -1,31 +1,31 @@
 export const users = [
     {
       "username": "Jarsher",
-      "password": "$2a$08$wilSc/9mGDQYiDRGDjFjR.2tba3XM5227IBI5WDqIroLRtp.iRgue",
+      "password": "password",
       "email": "jarsher@email.com",
       "bio": "Here we go again! WOOOT"
     },
     {
       "username": "Hooplah",
-      "password": "$2a$08$wilSc/9mGDQYiDRGDjFjR.2tba3XM5227IBI5WDqIroLRtp.iRgue",
+      "password": "password",
       "email": "hooplah@email.com",
       "bio": "I am the best, around! No one is ever going to bring me down!"
     },
     {
       "username": "Marsh",
-      "password": "$2a$08$wilSc/9mGDQYiDRGDjFjR.2tba3XM5227IBI5WDqIroLRtp.iRgue",
+      "password": "password",
       "email": "marsh@email.com",
       "bio": ""
     },
     {
       "username": "Tercher",
-      "password": "$2a$08$wilSc/9mGDQYiDRGDjFjR.2tba3XM5227IBI5WDqIroLRtp.iRgue",
+      "password": "password",
       "email": "turch@email.com",
       "bio": "Some bs bio that is super duper long but within the limit."
     },
     {
       "username": "Blarfing",
-      "password": "$2a$08$wilSc/9mGDQYiDRGDjFjR.2tba3XM5227IBI5WDqIroLRtp.iRgue",
+      "password": "password",
       "email": "blarf@email.com",
       "bio": "Poooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooop."
     }
